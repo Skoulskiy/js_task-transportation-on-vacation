a0041ce3ab1e4557d48836120efa52f74ae0280f
Skoulskiy <ogurethcs@gmail.com>
2026-06-16 21:23:42 +0300

    Added new variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,14 +6,16 @@
 function calculateRentalCost(days) {
   const dayPrice = 40;
   const longTerm = 7;
+  const longTermDiscount = 50;
   const shortTerm = 3;
+  const shortTermDiscount = 20;
 
   if (days >= longTerm) {
-    return days * dayPrice - 50;
+    return days * dayPrice - longTermDiscount;
   }
 
   if (days >= shortTerm) {
-    return days * dayPrice - 20;
+    return days * dayPrice - shortTermDiscount;
   }
 
   return days * dayPrice;
